a4e9ec7f70abcf71d0a694ae583d6fb24176d9ef
JimmyThreeSticks <james.a.root.3@gmail.com>
2025-11-07 17:37:33 +0000

    Pending changes exported from your codespace

---
+++ src/books/Book.jsx
@@ -0,0 +1,14 @@
+function Book({ title, author }) {
+
+
+    return (
+            <tr>
+                <td>
+                    {title}
+                </td>
+                <td>{author}</td>
+            </tr>
+            );
+}
+
+export default Book;
